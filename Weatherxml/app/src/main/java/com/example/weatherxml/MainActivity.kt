c349b117780326fe4a16a006a292a77f69d305d7
Илья <ilyakirpichev@yandex.ru>
2020-10-07 04:45:08 +0300

    some fixed

--- Weatherxml/app/src/main/java/com/example/weatherxml/MainActivity.kt
+++ Weatherxml/app/src/main/java/com/example/weatherxml/MainActivity.kt
@@ -2,20 +2,11 @@ package com.example.weatherxml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val switcher = findViewById<Switch>(R.id.theme);
-
-        switcher.setOnCheckedChangeListener({ _ , isChecked ->
-            val message = if (isChecked) "Switch1:ON" else "Switch1:OFF"
-            System.out.println(message);
-        })
     }
 
 
